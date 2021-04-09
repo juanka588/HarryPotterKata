3dd6e020653dd74c1bbccd41a955130522d7e908
Juan Camilo Rodriguez Duran <juan.duran@salesforce.com>
2021-04-09 13:27:38 +0200

    move the project to gradle add parameterized test example

--- src/test/java/domain/BasketTest.java
+++ src/test/java/domain/BasketTest.java
@@ -1,13 +1,40 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BasketTest {
 
-    @org.junit.jupiter.api.Test
-    void getPrice() {
+    @Test
+    void getPrice_givenEmptyBasket() {
         Basket sut = new Basket();
-        double current = sut.getPrice(new Book("hp1"));
-        assertEquals(2.0, current);
+        double current = sut.getPrice();
+        assertEquals(0.0, current);
     }
+
+    /**
+     * Demo test for parameterized test
+     */
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    private static class Calculator {
+        public int add(int first, int second) {
+            return first + second;
+        }
+    }
+
 }
