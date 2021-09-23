039ce07853a2f6e36dca94d53f8ab4e957286f9b
Juan Camilo Rodriguez Duran <juan.duran@salesforce.com>
2021-09-23 14:54:28 +0200

    price without discount

--- src/main/java/domain/Basket.java
+++ src/main/java/domain/Basket.java
@@ -1,7 +1,9 @@
 package domain;
 
 public class Basket {
-    public double getPrice(Book... books){
-        return 0.0;
+    public static final double SHELF_PRICE = 8.0d;
+
+    public double getPrice(Book... books) {
+        return books.length * SHELF_PRICE;
     }
 }
--- src/test/java/domain/BasketTest.java
+++ src/test/java/domain/BasketTest.java
@@ -1,13 +1,38 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BasketTest {
 
     @org.junit.jupiter.api.Test
-    void getPrice() {
+    void givenEmptyBasketThenPriceMustBeZero() {
+        // Arrange
         Basket sut = new Basket();
-        double current = sut.getPrice(new Book("hp1"));
-        assertEquals(2.0, current);
+        // Act
+        double totalPrice = sut.getPrice();
+        // Assert
+        assertEquals(0.0, totalPrice);
+    }
+
+    @org.junit.jupiter.api.Test
+    void givenOneBookInBasketThenPriceIsShelfPrice() {
+        // Arrange
+        Basket sut = new Basket();
+        // Act
+        double totalPrice = sut.getPrice(new Book("hp1"));
+        // Assert
+        assertEquals(8.0, totalPrice);
+    }
+
+    @org.junit.jupiter.api.Test
+    void givenTwoIdenticalBooksInBasketThenPriceIsShelfPrice() {
+        // Arrange
+        Basket sut = new Basket();
+        // Act
+        Book hp1 = new Book("hp1");
+        Book hp1Copy = new Book("hp1");
+        double totalPrice = sut.getPrice(hp1, hp1Copy);
+        // Assert
+        assertEquals(2 * 8.0, totalPrice);
     }
 }
