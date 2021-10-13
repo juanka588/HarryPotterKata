98375b8a6d1422631eb529d6cdae51fec73d1cc3
Sébastien Colladon <scolladon@salesforce.com>
2021-10-13 17:10:47 +0200

    refactor: adding discount rate

--- src/main/java/domain/Basket.java
+++ src/main/java/domain/Basket.java
@@ -6,10 +6,16 @@ public class Basket {
 
     public static final double BASE_PRICE = 8.0;
 
-    public double getPrice(Book... books){
-        if(books.length > 1 && Arrays.stream(books).map(Book::getTitle).distinct().count() == books.length) {
-            return books.length * BASE_PRICE * .95;
+    public double getPrice(Book... books) {
+        return books.length * BASE_PRICE * getDiscountRate(books);
+    }
+
+    public double getDiscountRate(final Book... books) {
+        if(books.length == 1) {
+            return 1.0;
+        } else if (Arrays.stream(books).map(Book::getTitle).distinct().count() == books.length) {
+            return 0.95;
         }
-        return books.length * BASE_PRICE;
+        return 1.0;
     }
 }
