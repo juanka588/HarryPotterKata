dcd2486b3fe8df42edd393719ae3bd9f8688d544
Simon Kempf <skempf@salesforce.com>
2021-10-13 16:40:01 +0200

    refacto: introduce constant for pricing

--- src/main/java/domain/Basket.java
+++ src/main/java/domain/Basket.java
@@ -1,7 +1,10 @@
 package domain;
 
 public class Basket {
+
+    public static final double BASE_PRICE = 8.0;
+
     public double getPrice(Book... books){
-        return books.length * 8.0;
+        return books.length * BASE_PRICE;
     }
 }
--- src/test/java/domain/BasketTest.java
+++ src/test/java/domain/BasketTest.java
@@ -2,19 +2,21 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 class BasketTest {
 
     @org.junit.jupiter.api.Test
     void getPrice_GivenOneBook() {
         Basket sut = new Basket();
         double current = sut.getPrice(new Book("hp1"));
-        assertEquals(8.0, current);
+        assertEquals(Basket.BASE_PRICE, current);
     }
 
     @org.junit.jupiter.api.Test
     void getPrice_GivenTwiceTheSameBook() {
         Basket sut = new Basket();
         double current = sut.getPrice(new Book("hp1"), new Book("hp1"));
-        assertEquals(16.0, current);
+        assertEquals(Basket.BASE_PRICE*2, current);
     }
 }
