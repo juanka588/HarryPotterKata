1b559d5fb63d5d4bfe23b3e5735fed886ec3fbe7
Simon Kempf <skempf@salesforce.com>
2021-10-13 17:17:12 +0200

    feat: extracting method to have distinct number of books

--- src/main/java/domain/Basket.java
+++ src/main/java/domain/Basket.java
@@ -13,11 +13,15 @@ public class Basket {
     public double getDiscountRate(final Book... books) {
         if(books.length == 1) {
             return 1.0;
-        } else if (books.length == 2 && Arrays.stream(books).map(Book::getTitle).distinct().count() == books.length) {
+        } else if (books.length == 2 && getDistinctBookNumber(books) == books.length) {
             return 0.95;
-        } else if (books.length == 3 && Arrays.stream(books).map(Book::getTitle).distinct().count() == books.length) {
+        } else if (books.length == 3 && getDistinctBookNumber(books) == books.length) {
             return 0.90;
         }
         return 1.0;
+    }
+
+    private long getDistinctBookNumber(Book[] books) {
+        return Arrays.stream(books).map(Book::getTitle).distinct().count();
     }
 }
