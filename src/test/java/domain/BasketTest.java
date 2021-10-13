5c1d9925c9b26cbe580ae2760882ee59dda1d9ff
Antoine Rosenbach <arosenbach@salesforce.com>
2021-10-13 16:50:29 +0200

    refacto: test names

--- src/test/java/domain/BasketTest.java
+++ src/test/java/domain/BasketTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class BasketTest {
 
     @org.junit.jupiter.api.Test
-    void getPrice_GivenOneBook() {
+    void getPrice_GivenOneBook_ThenReturnsBasePrice() {
         Basket sut = new Basket();
         double current = sut.getPrice(new Book("hp1"));
         assertEquals(Basket.BASE_PRICE, current);
     }
 
     @org.junit.jupiter.api.Test
-    void getPrice_GivenTwiceTheSameBook() {
+    void getPrice_GivenTwiceTheSameBook_ThenReturnsTwiceTheBasePrice() {
         Basket sut = new Basket();
         double current = sut.getPrice(new Book("hp1"), new Book("hp1"));
         assertEquals(Basket.BASE_PRICE*2, current);
